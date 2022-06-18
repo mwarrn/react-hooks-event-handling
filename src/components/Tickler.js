6df6e4b41b81f8744eef50f2be78f201fcea218a
Michael Warren <mwarrn@amazon.com>
2022-06-18 06:43:12 -0700

    Completed assignment

--- src/components/Tickler.js
+++ src/components/Tickler.js
@@ -5,7 +5,15 @@ function Tickler() {
     console.log("Teehee!");
   }
 
-  return <button>Tickle me!</button>;
+  return <button onClick={tickle}>Tickle me!</button>;
 }
+
+// function Tickler() {
+//   function tickle(event) {
+//     console.log(event);
+//   }
+
+//   return <button onClick={tickle}>Tickle me!</button>;
+// }
 
 export default Tickler;
